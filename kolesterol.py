a2ec84aab603cf21b3f115ed43b992fa09a10092
elvioaldwin <101845470+elvioaldwin@users.noreply.github.com>
2024-05-08 20:37:03 +0700

    Update kolesterol.py

--- kolesterol.py
+++ kolesterol.py
@@ -5,22 +5,22 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #add8e6;  /* Kode warna hex untuk biru muda */
-        color: #ffffff;             /* Kode warna hex untuk putih */
-        font-size: 18px;            /* Ukuran font diperbesar */
+        background-color: #f3e9df;  /* Warna latar belakang baru */
+        color: #c99548;             /* Warna teks baru */
+        font-size: 18px;            /* Ukuran font tetap */
     }
     .css-1d391kg {
-        color: #ffffff;             /* Mengatur warna teks untuk elemen tertentu */
+        color: #c99548;             /* Mengatur warna teks untuk elemen tertentu */
     }
     table {
-        border: 2px solid white;    /* Garis tabel berwarna putih */
+        border: 2px solid #fb8e54;  /* Garis tabel dengan warna baru */
     }
     th {
-        background-color: #ff6347;  /* Warna latar untuk header tabel */
+        background-color: #fb8e54;  /* Warna latar untuk header tabel */
         color: white;               /* Warna teks untuk header tabel */
     }
     td {
-        background-color: #ffe4e1;  /* Warna latar untuk sel tabel */
+        background-color: #f3e9df;  /* Warna latar untuk sel tabel */
         color: black;               /* Warna teks untuk sel tabel */
     }
     </style>
